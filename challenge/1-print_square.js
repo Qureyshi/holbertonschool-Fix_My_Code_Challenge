f6682b13b96a7587055b7dd3802aeea5eab61201
Qureyshi <kamranqureyshi1998@gmail.com>
2024-09-13 15:47:01 +0000

    fixcode

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -17,9 +17,15 @@ const size = parseInt(process.argv[2], 10);
 
 if (isNaN(size) || size <= 0) {
     process.stderr.write("Invalid size\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
 }
 
 for (let i = 0; i < size; i++) {
-    console.log("#".repeat(size));
+    for (let j = 0; j < size; j++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
 }
+
